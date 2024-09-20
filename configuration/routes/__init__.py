c94be40239153b72a8f4e329aee78cf021723a8e
NikolayMakovetsky <nicholasmakovetsky@gmail.com>
2024-09-20 22:26:23 +0300

    rotes/__init__.py -> fix Rotes attribute

--- configuration/routes/__init__.py
+++ configuration/routes/__init__.py
@@ -1,4 +1,4 @@
 from app.configuration.routes.routes import Routes
 from app.internal.routes import user
 
-__routes__ = Routes(routes=(user.router, )) # все, механизм регистрации тут готов
+__routes__ = Routes(routers=(user.router, )) # все, механизм регистрации тут готов
