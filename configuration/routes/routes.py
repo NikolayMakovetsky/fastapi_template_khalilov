d401dc266c26d76d61571a21dcf0665663934707
NikolayMakovetsky <nicholasmakovetsky@gmail.com>
2024-09-20 18:12:13 +0300

    create routes/routes.py

---
+++ configuration/routes/routes.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from fastapi import FastAPI
+
+
+# создаем именно dataclass т.к. он будет принимать и хранить в атрибутах определенные пути,
+# которые мы будем затем регистрировать
+
+
+@dataclass(frozen=True)  # frozen class неизменяемый
+class Routes:
+    routes: tuple
+
+    def register_routes(self, app: FastAPI):
+        """Функция регистрирующая роутер в наше приложение"""
+
+        for router in self.routes:
+            app.include_router(router)
