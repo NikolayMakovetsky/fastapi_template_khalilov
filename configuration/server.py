13db39e2958c2713bf1738df968f6f38b2ce7812
NikolayMakovetsky <nicholasmakovetsky@gmail.com>
2024-09-20 22:01:31 +0300

    server.py -> add comment about __register_events(app)

--- configuration/server.py
+++ configuration/server.py
@@ -17,6 +17,9 @@ class Server:
     @staticmethod
     def __register_events(app):
         """Приватный статический метод регистрации событий"""
+        # По аналогии с __register_routes(app) можно реализовать и этот метод
+        # app.on_event('startup')()
+        # из нужного модуля импортируем сюда ф-ю кот. будет выполнятся при запуске нашего fastapi-сервиса
         ...
 
     @staticmethod
